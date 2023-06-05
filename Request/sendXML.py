63b4137824f39942071f189a0c4036d1aebec77b
I3run01 <brunnooa.v@gmail.com>
2023-06-05 18:03:13 -0300

    send file

--- Request/sendXML.py
+++ Request/sendXML.py
@@ -1,9 +1,9 @@
 import requests
 
 url = 'http://localhost:5000'
-def sendXML(xml_data):
-    headers = {'Content-Type': 'application/xml'}
-    response = requests.post(url, data=xml_data, headers=headers)
+
+def sendXML(files):
+    response = requests.post(url, files=files)
     return response.json()
 
 
--- main.py
+++ main.py
@@ -4,9 +4,10 @@ xmlFile = str(input('put the file name: '))+'.xml'
 
 print(xmlFile)
 
-with open(xmlFile, 'r') as xml_file:
-    xml_data = xml_file.read()
-
-res = req.sendXML(xml_data)
+with open(xmlFile, 'rb') as f:
+    files = {'file': f}
+    res = req.sendXML(files)
 print(res)
+
+
 
