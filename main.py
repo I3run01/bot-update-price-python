d5c07bc2835cca08a3c5d2ca481835d61220337b
I3run01 <brunnooa.v@gmail.com>
2023-06-06 16:56:48 -0300

    name business returned

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import Request.sendXML as req
-import json
 
 xmlFile = str(input('put the file name: '))+'.xml'
-
-print(xmlFile)
 
 with open(xmlFile, 'rb') as f:
     files = {'file': f}
     res = req.sendXML(files)
 
-print(res['cnpj'])
+print(res)
 
 
 
