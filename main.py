c532cfa2ce64fd03cbc106e2eed34e95857cb2c7
I3run01 <brunnooa.v@gmail.com>
2023-06-08 09:28:29 -0300

    print improved

--- main.py
+++ main.py
@@ -6,7 +6,19 @@ with open(xmlFile, 'rb') as f:
     files = {'file': f}
     res = req.sendXML(files)
 
-print(res)
+c=0
+for products in res['products']:
+    print(c)
+    print(f"cProd: {products['cProd']}")
+    print(f"cEAN: {products['cEAN']}")
+    print(f"nfeName: {products['nfeName']}")
+    print(f"ncm: {products['ncm']}")
+    print(f"costPrice: {products['costPrice']}")
+    print(f"comercialName: {products['comercialName']}")
+    print('')
+
+    c = c+1
+
 
 
 
