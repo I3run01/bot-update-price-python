13092c435dca2943be219811c14219f31c75c5e7
I3run01 <brunnooa.v@gmail.com>
2023-06-11 14:56:18 -0300

    products class created

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import Request.sendXML as req
 import utils.defs as defs
 
+products_list = []
+
 xmlFile = str(input('put the file name: '))+'.xml'
 
 with open(xmlFile, 'rb') as f:
@@ -9,4 +11,13 @@ with open(xmlFile, 'rb') as f:
 
 csv_path = f'database/{res["name"]}.csv'
 
+products = res['products']
+
 defs.create_csv_if_not_exists(csv_path)
+
+for product in products:
+    datas = defs.get_row_by_cEAN(csv_path, product['cEAN'])
+
+    if(datas == None):
+        print('no datas received')
+        print("")
--- utils/defs.py
+++ utils/defs.py
@@ -20,5 +20,12 @@ def create_csv_if_not_exists(file_path):
         df = pd.DataFrame(columns=columns)
         df.to_csv(file_path, index=False)
         print(f"CSV file created at: {file_path}")
+
+def get_row_by_cEAN(file_path, cEAN):
+    df = pd.read_csv(file_path)
+
+    if cEAN in df['cEAN'].values:
+        row = df[df['cEAN'] == cEAN]
+        return row.to_dict(orient='records')[0]
     else:
-        print(f"CSV file already exists at: {file_path}")
+        return None
