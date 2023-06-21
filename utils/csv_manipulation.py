1bd9b14098b357d38a3082991927efc4a07a34d1
I3run01 <brunnooa.v@gmail.com>
2023-06-21 14:55:47 -0300

    working in df

--- utils/csv_manipulation.py
+++ utils/csv_manipulation.py
@@ -28,7 +28,6 @@ def get_row_by_cEAN(file_path, cEAN):
         return None
 
 def update_row(file_path, product: object):
-    df = pd.read_csv(file_path)
 
     current_date = datetime.now()
 
@@ -40,10 +39,24 @@ def update_row(file_path, product: object):
 
     # TODO: read this article: https://www.geeksforgeeks.org/update-column-value-of-csv-in-python/
 
-    if set(product.c_ean).intersection(set(df['cEAN'].values)):
-        df.loc[df['cEAN'] == product.c_ean, 'ours_code'] = product.ours_code
-        df.loc[df['cEAN'] == product.c_ean, 'selling_price'] = product.new_selling_price
-        df.loc[df['cEAN'] == product.c_ean, 'date_of_last_update'] = current_date
+    df = pd.read_csv(file_path)
+
+    rows_number = len(df)
+
+    product_index = None
+
+    for row_number in range(0, rows_number):
+        dataframe_cEAN = str(df.loc[row_number, "cEAN"])
+        product_cEAN = str(product.c_ean)
+
+        if(dataframe_cEAN == product_cEAN):        
+            product_index = row_number
+            break
+    
+    # TODO: Correct the following if
+    if(product_index):
+        df.loc[product_index, 'selling_price'] = product.new_selling_price
+        df.loc[product_index, 'date_of_last_update'] = current_date
     else:
         new_row = {
             'ours_code': product.ours_code, 
@@ -54,7 +67,7 @@ def update_row(file_path, product: object):
             'nfe_name': product.nfe_name
         }
         df = df.append(new_row, ignore_index=True)
-    
+
     df.to_csv(file_path, index=False)
 
     return df
