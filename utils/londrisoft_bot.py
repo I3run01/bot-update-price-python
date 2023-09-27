7fd22bd54b7818394a6214c8c24e42fcd70a4cba
I3run01 <brunnooa.v@gmail.com>
2023-09-27 14:39:24 -0300

    Update londrisoft_bot.py

--- utils/londrisoft_bot.py
+++ utils/londrisoft_bot.py
@@ -258,7 +258,7 @@ def update_and_print_products(products: list, status: Union[Literal['increase'],
 
     pyautogui.click(200,650)
     pyautogui.click(200,360)
-    
+
     for product in products:
 
         if(
@@ -291,7 +291,6 @@ def update_and_print_products(products: list, status: Union[Literal['increase'],
         if(product.print_product):
             our_code_print_list.append(product.ours_code)
         
-
     print_labels(our_code_print_list)
 
 def just_update_products(products: list, status: Union[Literal['increase'], Literal['any']]):
@@ -302,28 +301,22 @@ def just_update_products(products: list, status: Union[Literal['increase'], Lite
     pyautogui.click(200,360)
 
     for product in products:
+        if(
+            status == 'increase' and 
+            product.old_selling_price >= product.new_selling_price and 
+            product.ours_code and
+            is_product_created_today(product.ours_code) == False
+        ):    
+            product.print_product = False
+            continue
 
-        if(status == 'increase'):
-
-            if(product.old_selling_price > product.new_selling_price and product.ours_code):
-                continue
-
-            has_product_internally = has_product_in_LS(product)
-           
-            if(has_product_internally):
-                update_product_price(product, status)
-
-            else:
-                create_product(product)
+        has_product_internally = has_product_in_LS(product)
         
-        else: 
-            has_product_internally = has_product_in_LS(product)
-           
-            if(has_product_internally):
-                update_product_price(product, status)
+        if(has_product_internally):
+            update_product_price(product, status)
 
-            else:
-                create_product(product)
+        else:
+            create_product(product)
                 
 def just_print_products(products: list, status: Union[Literal['increase'], Literal['any']]):
 
